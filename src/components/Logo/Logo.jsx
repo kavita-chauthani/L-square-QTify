ff78c8699dd7ea52aaf9ce185d47d7dba7712faa
kavita chauthani <kckabu81@gmail.com>
2023-10-09 16:23:30 +0530

    C3:Button

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Logoimage from "../assets/logoimage.png";
+
+const Logo = () => {
+  return <img src={Logoimage} alt="logo" width={67} />;
+};
+
+export default Logo;
