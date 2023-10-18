ff4457c25c8d4310f7a9d387186dabfae4fcf857
kavita chauthani <kckabu81@gmail.com>
2023-10-18 21:45:07 +0530

    grid-qtify-takehome

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Card.module.css";
-import { Chip, Grid, Tooltip } from "@mui/material";
+import { Chip, Tooltip } from "@mui/material";
 
 const Card = ({ data, type }) => {
   const getCard = (type) => {
